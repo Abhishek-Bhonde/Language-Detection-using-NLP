6aaf044d338ddd28983a812ca0049f67e38d4921
Abhishek Bhonde <121537842+Abhishek-Bhonde@users.noreply.github.com>
2023-12-25 13:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def home():
     return render_template("main.html")
 
-@app.route("/predict", methods = ["POST"])
+@app.route('/predict', methods = ["POST"])
 
 def predict():
     # loading the dataset
